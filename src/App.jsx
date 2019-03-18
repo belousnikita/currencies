0e0e8177cef0dd444ce18a84f3e0bec34c5a1fea
Nikita Belous <belousnikita@gmail.com>
2019-03-18 22:18:17 +0200

    App created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import Button from './Button';
+import classnames from 'classnames';
+import './styles.scss';
+
+const header = (handler, state) => {
+    const { isCurrencies, isConverter } = state;
+    return <div className="Head">
+        <div className="Currencies">
+            <Button type="currnecies" text="Currencies" onClick={() => handler("currencies")} active={isCurrencies}>
+                Currencies
+            <i class="fas fa-money-bill-alt" ></i>
+            </Button>
+        </div>
+        <div className="Converter">
+            <Button type="converter" text="Converter" onClick={() => handler("converter")} active={isConverter}>
+                Converter
+            <i class="fas fa-balance-scale" ></i>
+            </Button>
+        </div>
+    </div>
+};
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isCurrencies: true,
+            isConverter: false,
+        }
+    }
+    selectorHandler(type) {
+        switch (type) {
+            case "currencies": {
+                this.setState({ isCurrencies: true, isConverter: false });
+                return;
+            }
+            case "converter": {
+                this.setState({ isCurrencies: false, isConverter: true });
+                return;
+            }
+            default: return;
+        }
+    }
     render() {
-        return <div>Hello World</div>
+        const { isCurrencies, isConverter } = this.state;
+        const blockClassName = classnames('block', {
+            left: isCurrencies,
+            right: isConverter
+        });
+        const blockCornerClassName = classnames('corner', {
+            left: isCurrencies,
+            right: isConverter
+        });
+        return <div className="grid-container">
+            {header(this.selectorHandler.bind(this), this.state)}
+            <div className={blockClassName}>
+                <div className={blockCornerClassName}></div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam tempora, animi consequuntur excepturi atque odit illum odio aspernatur cupiditate autem earum accusantium natus doloremqu
+                e adipisci error voluptatum fugit omnis blanditiis.</p>
+            </div>
+        </div>
     }
 }
