2d5cffa1c94ac042ba54b674bb909b17a6c6a0e6
Nikita Belous <belousnikita@gmail.com>
2019-03-21 19:53:53 +0200

    App structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Button from './Button';
-import classnames from 'classnames';
-import './styles.scss';
+import axios from 'axios';
+import Currency from './Currency';
+import Block from './Block';
+import Rainbow from 'rainbowvis.js';
+import './styles/styles.scss';
 
 const header = (handler, state) => {
     const { isCurrencies, isConverter } = state;
-    return <div className="Head">
+    return <div className="head">
         <div className="Currencies">
-            <Button type="currnecies" text="Currencies" onClick={() => handler("currencies")} active={isCurrencies}>
-                Currencies
-            <i class="fas fa-money-bill-alt" ></i>
+            <Button type="currnecies" text="Курс" onClick={() => handler("currencies")} active={isCurrencies}>
+                Курс
+            <i className="fas fa-money-bill-alt" ></i>
             </Button>
         </div>
         <div className="Converter">
-            <Button type="converter" text="Converter" onClick={() => handler("converter")} active={isConverter}>
-                Converter
-            <i class="fas fa-balance-scale" ></i>
+            <Button type="converter" text="Конвертер" onClick={() => handler("converter")} active={isConverter}>
+                Конвертер
+            <i className="fas fa-balance-scale" ></i>
             </Button>
         </div>
     </div>
@@ -24,10 +27,37 @@ const header = (handler, state) => {
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+        this.colorPalette = new Rainbow();
+        this.colorPalette.setSpectrum("#24346D", "#ea6970", "#93032E", "#AD7A99", "#034C3C", "#7FB7BE");
+        this.colorPalette.setNumberRange(0, 14);
         this.state = {
+            loaded: false,
             isCurrencies: true,
             isConverter: false,
+            currencies: ["EUR", "USD", "RUB", "GBP", "DKK", "CHF", "PLN", "CAD", "SEK", "KZT", "BGN", "CNY", "HKD", "BRL"]
         }
+    }
+    selectCurrency(i) {
+        const { currencies } = this.state;
+        if (currencies[i]) {
+            currencies[i].isSelected = !currencies[i].isSelected;
+            this.setState({ currencies });
+        }
+    }
+    componentWillMount() {
+        const today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0');
+        const yyyy = today.getFullYear();
+        const todayStr = `${yyyy}${mm}${dd}`;
+        const currencies = this.state.currencies.map(async (c, i) =>
+            axios.get(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${c}&date=${todayStr}&json`)
+                .then(response => {
+                    const data = response.data[0]
+                    return { ...data, color: `#${this.colorPalette.colorAt(i)}`, isSelected: false }
+                })
+                .catch(error => console.log(error)));
+        Promise.all(currencies).then((completed) => this.setState({ currencies: completed, loaded: true }));
     }
     selectorHandler(type) {
         switch (type) {
@@ -43,22 +73,38 @@ export default class App extends React.Component {
         }
     }
     render() {
-        const { isCurrencies, isConverter } = this.state;
-        const blockClassName = classnames('block', {
-            left: isCurrencies,
-            right: isConverter
-        });
-        const blockCornerClassName = classnames('corner', {
-            left: isCurrencies,
-            right: isConverter
-        });
+        const { isCurrencies, isConverter, currencies, loaded } = this.state;
         return <div className="grid-container">
             {header(this.selectorHandler.bind(this), this.state)}
-            <div className={blockClassName}>
-                <div className={blockCornerClassName}></div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam tempora, animi consequuntur excepturi atque odit illum odio aspernatur cupiditate autem earum accusantium natus doloremqu
-                e adipisci error voluptatum fugit omnis blanditiis.</p>
-            </div>
-        </div>
+            <Block first left={isCurrencies} right={isConverter} color="#4A4DA5" zIndex={currencies.length + 1}>
+                <div className="currency">Обрані валюти з'являться у конверторі</div>
+            </Block>
+            {loaded && isCurrencies && currencies.map((c, i) => {
+                if (c)
+                    return <Block key={i} left={isCurrencies} right={isConverter} color={c.color} zIndex={currencies.length - i}>
+                        <Currency
+                            name={c.cc}
+                            fullName={c.txt}
+                            buy="12.200000"
+                            sell="12.2123211"
+                            active={c.isSelected}
+                            onClick={() => this.selectCurrency(i)}></Currency>
+                    </Block>
+                else return null;
+            })}
+            {loaded && isConverter && currencies.map((c, i) => {
+                if (c && c.isSelected)
+                    return <Block key={i} left={isCurrencies} right={isConverter} color={c.color} zIndex={currencies.length - i}>
+                        <Currency
+                            name={c.cc}
+                            fullName={c.txt}
+                            buy="12.200000"
+                            sell="12.2123211"
+                            active={c.isSelected}
+                            onClick={() => this.selectCurrency(i)}></Currency>
+                    </Block>
+                else return null;
+            })}
+        </div >
     }
 }
