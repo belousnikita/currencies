5a35392213bf8de6876c663be0eca0e2701efbd1
Nikita Belous <belousnikita@gmail.com>
2019-03-21 19:52:18 +0200

    Fav button added

--- src/Currency.jsx
+++ src/Currency.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Button from './Button';
 export default class Currency extends React.Component {
     constructor(props) {
         super(props);
         this.name = props.name;
         this.buy = props.buy;
         this.sell = props.sell;
+        this.onClick = props.onClick;
     }
     render() {
         return <div className="currency">
@@ -15,6 +16,8 @@ export default class Currency extends React.Component {
                 /
                 <div className="currency_value_sell">{this.sell}</div>
             </div>
+            <Button className="button favorite" active={this.props.active} onClick={this.onClick}>
+                <i className="fas fa-star"></i></Button>
         </div>
     }
 }
