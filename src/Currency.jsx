ad468f5f493619c111ce40ddb85464cb27bd0402
Nikita Belous <belousnikita@gmail.com>
2019-03-21 20:38:42 +0200

    Currency numbers added

--- src/Currency.jsx
+++ src/Currency.jsx
@@ -4,17 +4,19 @@ export default class Currency extends React.Component {
     constructor(props) {
         super(props);
         this.name = props.name;
+        this.fullName = props.fullName;
         this.buy = props.buy;
         this.sell = props.sell;
         this.onClick = props.onClick;
     }
     render() {
         return <div className="currency">
+            <div className="currency_fullname">{this.fullName}</div>
             <div className="currency_name">{this.name}</div>
             <div className="currency_value">
-                <div className="currency_value_buy">{this.buy}</div>
-                /
-                <div className="currency_value_sell">{this.sell}</div>
+                <div className="currency_value_buy">{`${this.buy} ${this.name}`}</div>
+                |
+                <div className="currency_value_sell">{`${this.sell} UAH`}</div>
             </div>
             <Button className="button favorite" active={this.props.active} onClick={this.onClick}>
                 <i className="fas fa-star"></i></Button>
