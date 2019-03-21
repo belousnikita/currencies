85b50275a955a0451ba8a645b06b6aa018e71e84
Nikita Belous <belousnikita@gmail.com>
2019-03-21 19:53:29 +0200

    Prop text removed

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,16 +3,15 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 export default class Button extends React.Component {
     render() {
-        const { className, text, onClick, active, children } = this.props;
+        const { className, onClick, active, children } = this.props;
         const classNameResult = classNames(className || "button", {
             active: active || false,
         })
-        return <div className={classNameResult} onClick={onClick}>{children}</div>
+        return <button className={classNameResult} onClick={onClick}>{children}</button>
     }
 }
 Button.propTypes = {
     className: PropTypes.string,
-    text: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
     isActive: PropTypes.bool,
 }
