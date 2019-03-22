e2eb80d5e18d0e724fda725a8afb432261234088
Nikita Belous <belousnikita@gmail.com>
2019-03-22 18:59:13 +0200

    Updates

---
+++ precache-manifest.c824184fd5a92d14ba7d777ccb5b57dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a933b66f13698b35eff4",
+    "url": "/currencies/static/js/runtime~main.b73436df.js"
+  },
+  {
+    "revision": "e18c33fb5f047b5773dc",
+    "url": "/currencies/static/js/main.6279505a.chunk.js"
+  },
+  {
+    "revision": "c9870777f6f9e70af071",
+    "url": "/currencies/static/js/2.74da740c.chunk.js"
+  },
+  {
+    "revision": "e18c33fb5f047b5773dc",
+    "url": "/currencies/static/css/main.ca5fa316.chunk.css"
+  },
+  {
+    "revision": "85767c006559c7ccd08cff23c898e64f",
+    "url": "/currencies/index.html"
+  }
+];
